e0fa4cdd9559f35baf63ca7f926ee23aa8c17aac
Manoj18121812 <gavhaneam@gmail.com>
2025-11-22 17:04:20 +0530

    Service done

--- src/main/java/com/example/Order_Management_Rest_Api/service/OrderService.java
+++ src/main/java/com/example/Order_Management_Rest_Api/service/OrderService.java
@@ -1,8 +1,12 @@
 package com.example.Order_Management_Rest_Api.service;
 
+import com.example.Order_Management_Rest_Api.entity.Customer;
+import com.example.Order_Management_Rest_Api.entity.OrderEntity;
 import com.example.Order_Management_Rest_Api.repository.CustomerRepository;
 import com.example.Order_Management_Rest_Api.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -15,5 +19,37 @@ public class OrderService {
         this.orderRepo = orderRepo;
     }
 
+    public OrderEntity createOrder(Integer customerId, OrderEntity order){
+    Customer customer = customerRepo.findById(customerId).orElse(null);
 
+    if(customer == null){
+        return null;
+    }
+    order.setCustomer(customer);
+    return orderRepo.save(order);
+    }
+
+    public List<OrderEntity> getOrdersOfCustomer(Integer customerId){
+        return orderRepo.findByCustomerId(customerId);
+    }
+
+    public OrderEntity updateOrder(Integer id,OrderEntity updated){
+        OrderEntity old = orderRepo.findById(id).orElse(null);
+        if(old==null){
+            return null;
+        }
+        old.setQuantity(updated.getQuantity());
+        old.setPrice(updated.getPrice());
+        old.setProductName(updated.getProductName());
+
+        return orderRepo.save(old);
+    }
+
+    public boolean deleteOrder(Integer id){
+        if(!orderRepo.existsById(id)){
+            return false;
+        }
+        orderRepo.deleteById(id);
+        return true;
+    }
 }
