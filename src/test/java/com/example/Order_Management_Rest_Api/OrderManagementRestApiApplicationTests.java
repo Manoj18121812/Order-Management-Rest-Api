d6e38fd67cba08c90422fd16f7a09e884b7d2f86
Manoj18121812 <gavhaneam@gmail.com>
2025-11-22 16:50:41 +0530

    RestApi

---
+++ src/test/java/com/example/Order_Management_Rest_Api/OrderManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Order_Management_Rest_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
